644e9a1c8b366e84ea257c8cecae76533c707bec
Manish sah <sahmanish20@gmail.com>
2023-05-12 22:01:55 +0545

    use relative path and index route

--- vanlife/src/pages/Host/Dashboard.jsx
+++ vanlife/src/pages/Host/Dashboard.jsx
@@ -1,11 +1,9 @@
-import {Outlet} from "react-router-dom"
-
-export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    return (
         <>
-        <h1>Dashboard goes here</h1>
-        <Outlet />
+            <h1>Dashboard goes here</h1>
+            
         </>
-        
+
     )
 }
