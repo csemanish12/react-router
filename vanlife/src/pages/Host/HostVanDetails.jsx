1e8efe1fc6ea0214a8b97f7504f5cbc4008364fe
Manish sah <sahmanish20@gmail.com>
2023-05-21 11:06:05 +0545

    create hostvandetails page

--- vanlife/src/pages/Host/HostVanDetails.jsx
+++ vanlife/src/pages/Host/HostVanDetails.jsx
@@ -1,5 +1,27 @@
+import React from "react"
+import {useParams} from "react-router-dom" 
+
 export default function HostVanDetails(){
+    
+    const [currentVan, setCurrentVan] = React.useState(null)
+    const {id} = useParams()
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [])
+
+    if (!currentVan){
+        return <h1>Loading....</h1>
+    }
+    console.log("current van:", currentVan)
     return(
-        <h1>Host vans Detail here</h1>
+    <div>
+        <img src={currentVan.imageUrl} width={150} />
+        <h2>{currentVan.name}</h2>
+        <p>{currentVan.price}</p>
+        <p>{currentVan.type}</p>
+    </div>
     )
 }
